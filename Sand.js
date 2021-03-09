6a66727248ba0ff4e1e6d14966f32b1e8d1664b7
ambit741235 <76868481+ambit741235@users.noreply.github.com>
2021-03-09 17:07:16 +0530

    Add files via upload

---
+++ Sand.js
@@ -0,0 +1,26 @@
+class Sand{
+    constructor(x,y,r){
+        var options={
+            restitution:0.3,
+            friction:5,
+            density:1
+        }
+        this.body = Bodies.circle(x,y,r,options);
+        this.x = x;
+        this.y = y;
+        this.r = r;
+
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        push();
+        strokeWeight(3);
+        stroke("black");
+        translate(pos.x,pos.y);
+        ellipseMode(RADIUS);
+        fill("red");
+        ellipse(0,0,this.r);
+        pop();
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,61 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine, world;
+var stone, rubber, hammer, plane, iron;
+var sand1, sand2, sand3, sand4, sand5, sand6;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	plane = new Plane(400,700,1200,50);
+	rubber = new Rubber(700,500,30);
+	stone = new Stone(100,500,100,100);
+	hammer = new Hammer(50,50);
+
+	sand1 = new Sand(500,500,5);
+	sand2 = new Sand(200,500,5);
+	sand3 = new Sand(300,500,5);
+	sand4 = new Sand(400,500,5);
+	sand5 = new Sand(600,500,5);
+	sand6 = new Sand(400,500,5);
+
+	iron = new Iron(450,500,150,90);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+	background("lightblue");
+	Engine.update(engine);
+
+	plane.display();
+	rubber.display();
+	stone.display();
+	hammer.display();
+
+	sand1.display();
+	sand2.display();
+	sand3.display();
+	sand4.display();
+	sand5.display();
+	sand6.display();
+
+	iron.display();
+}
+
+
+
